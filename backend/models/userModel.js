599fc5e27ab846b29352b3380f5aca91639beddb
Elmosius <2272008@maranatha.ac.id>
2024-11-22 19:03:48 +0700

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,18 +29,17 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  console.log("Password setelah di-hash:", this.password);
   next();
 });
 
 // Validasi password
 userSchema.methods.matchPassword = async function (input) {
-  //   return await bcrypt.compare(input, this.password);
-  const isMatch = await bcrypt.compare(input, this.password);
-  console.log("Input password:", input);
-  console.log("Hashed password:", this.password);
-  console.log("Password match:", isMatch);
-  return isMatch;
+  return await bcrypt.compare(input, this.password);
+  //   const isMatch = await bcrypt.compare(input, this.password);
+  //   console.log("Input password:", input);
+  //   console.log("Hashed password:", this.password);
+  //   console.log("Password match:", isMatch);
+  //   return isMatch;
 };
 
 const User = mongoose.model("User", userSchema);
