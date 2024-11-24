cb2f3b7cbc48dca299ed5f2e14661c4be3655777
Elmosius <2272008@maranatha.ac.id>
2024-11-24 17:30:26 +0700

    updated routes role & user

--- backend/routes/roleRoute.js
+++ backend/routes/roleRoute.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getRoles } from "../controllers/roleController.js";
+import { createRole, deleteRole, getRoleById, getRoles, updateRole } from "../controllers/roleController.js";
+import { validateRole } from "../validations/roleValidation.js";
+import handleValidation from "../middlewares/vallidateMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getRoles);
-
+router.get("/:id", getRoleById);
+router.post("/", validateRole, handleValidation, createRole);
+router.put("/:id", validateRole, handleValidation, updateRole);
+router.delete("/:id", deleteRole);
 export default router;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from "../controllers/userController.js";
+import { validateUser } from "../validations/userValidation.js";
+import handleValidation from "../middlewares/vallidateMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.post("/", validateUser, handleValidation, createUser);
+router.put("/:id", validateUser, handleValidation, updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
