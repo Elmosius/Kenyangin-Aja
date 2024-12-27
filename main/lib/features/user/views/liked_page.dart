5c3b19d96ace6b538b2ad3e099a8608ccd76433f
Elmosius <2272008@maranatha.ac.id>
2024-12-27 17:28:30 +0700

    update: ganti font liked page

--- main/lib/features/user/views/liked_page.dart
+++ main/lib/features/user/views/liked_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:main/data/providers/auth_state_notifier.dart';
 import 'package:main/data/providers/favorite_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:main/core/themes/colors.dart';
 
 class LikedPage extends ConsumerWidget {
   const LikedPage({super.key});
@@ -17,72 +19,151 @@ class LikedPage extends ConsumerWidget {
     ref.read(favoriteProvider.notifier).fetchFavorites(userId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tempat yang kamu tandai!'),
-      ),
-      body: favoritesAsync.when(
-        data: (favorites) => favorites.isEmpty
-            ? const Center(child: Text("Kamu belum menandai tempat!"))
-            : GridView.builder(
-                itemCount: favorites.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
+      backgroundColor: const Color(0xFFF5F5F5),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Judul
+            Center(
+              child: Text(
+                "Tempat yang kamu tandai!",
+                style: GoogleFonts.inter(
+                  color: AppColors.hitam,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                itemBuilder: (context, index) {
-                  final food = favorites[index];
-                  return GestureDetector(
-                    onTap: () {
-                      context.push('/food_detail', extra: food);
-                    },
-                    child: Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            image: DecorationImage(
-                              image: NetworkImage(food.imageUrl),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 16,
-                          left: 8,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ),
+            const SizedBox(height: 24),
+            // Search Bar
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.filter_list),
+                  onPressed: () {},
+                ),
+                hintText: "Search menu, restaurant or etc",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            // Konten Grid
+            favoritesAsync.when(
+              data: (favorites) => favorites.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "Kamu belum menandai tempat!",
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    )
+                  : GridView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: favorites.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        childAspectRatio: 32 / 40,
+                      ),
+                      itemBuilder: (context, index) {
+                        final food = favorites[index];
+                        return GestureDetector(
+                          onTap: () {
+                            // context.push('/food_detail', extra: food);
+                          },
+                          child: Stack(
                             children: [
-                              Text(food.name,
-                                  style: const TextStyle(color: Colors.white)),
-                              Row(
-                                children: [
-                                  const Icon(Icons.star, color: Colors.orange),
-                                  Text("${food.rating}"),
-                                ],
+                              // Background image
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  image: DecorationImage(
+                                    colorFilter: ColorFilter.mode(
+                                      Colors.black.withOpacity(0.45),
+                                      BlendMode.darken,
+                                    ),
+                                    image: NetworkImage(food.imageUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              // Text & Rating
+                              Positioned(
+                                bottom: 16,
+                                left: 8,
+                                right: 8,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      food.name,
+                                      style: GoogleFonts.inter(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    const SizedBox(height: 4),
+                                    Row(
+                                      children: [
+                                        const Icon(Icons.star,
+                                            color: Colors.orange, size: 16),
+                                        const SizedBox(width: 4),
+                                        Text(
+                                          "${food.rating.toStringAsFixed(1)} ratings",
+                                          style: GoogleFonts.inter(
+                                            fontSize: 12,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // Favorite Icon
+                              Positioned(
+                                top: 8,
+                                right: 8,
+                                child: IconButton(
+                                  icon: const Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                  ),
+                                  onPressed: () {
+                                    ref
+                                        .read(favoriteProvider.notifier)
+                                        .removeFavorite(userId, food.id);
+                                  },
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                        Positioned(
-                          top: 8,
-                          right: 8,
-                          child: IconButton(
-                            icon: const Icon(Icons.favorite, color: Colors.red),
-                            onPressed: () {
-                              ref
-                                  .read(favoriteProvider.notifier)
-                                  .removeFavorite(userId, food.id);
-                            },
-                          ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
-                  );
-                },
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (error, stack) => Center(
+                child: Text(
+                  'Error: $error',
+                  style: const TextStyle(fontSize: 14, color: Colors.red),
+                ),
               ),
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('Error: $error')),
+            ),
+          ],
+        ),
       ),
     );
   }
