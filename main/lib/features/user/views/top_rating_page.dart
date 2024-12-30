8ed90aade4ea68aa6fcc76e1e25939eb96b141cb
Elmosius <2272008@maranatha.ac.id>
2024-12-30 16:00:15 +0700

    Update top_rating_page.dart

--- main/lib/features/user/views/top_rating_page.dart
+++ main/lib/features/user/views/top_rating_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:main/core/widgets/food_grid.dart';
 import 'package:main/core/widgets/search_bar.dart';
+import 'package:main/core/widgets/title.dart';
 import 'package:main/data/providers/city_provider.dart';
 import 'package:main/data/providers/food_provider.dart';
 
@@ -44,14 +45,7 @@ class _TopRatingPageState extends State<TopRatingPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "Top Rating",
-                      style: GoogleFonts.inter(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                    const TitleWidget(title: "Top Rating"),
                     const SizedBox(height: 24),
                     SearchBarWidget(
                       onSearch: (query) {
